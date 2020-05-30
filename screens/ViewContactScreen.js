4658140d57498601c84bd0fd4f2faea6a178d991
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-05-30 17:22:42 +0530

    Populate data

--- screens/ViewContactScreen.js
+++ screens/ViewContactScreen.js
@@ -1,8 +1,30 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, 
+  TouchableOpacity, Linking, Platform, Alert, AsyncStorage } from 'react-native';
+import {Card, CardItem} from 'native-base';
+import {Entypo} from '@expo/vector-icons';
 
 
 export default class ViewContactScreen extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      fname:'dummy',
+      lname:'dummy',
+      phone:'dummy',
+      email:'dummy',
+      city:'dummy',
+      key:'dummy'
+    };
+  }
+
+  componentDidMount(){
+    this.props.navigation.addListener('focus',()=>{
+      let key=this.props.route.params.key;
+      
+    });
+  }
+
   render(){
     return(
       <View style={styles.container}>
